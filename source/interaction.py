45abfcba9e90ab6bc668d3e3729184363cc96b4f
Andrei <matovnikov04@mail.ru>
2022-04-14 11:35:22 +0300

    added first version of interaction

---
+++ source/interaction.py
@@ -0,0 +1,16 @@
+import bot
+from sqlite3 import users, standard_events
+
+class Interaction:
+    table = []
+    def find_user(self,id):
+        return users['id']
+
+    def add_personal_event(self, user):
+        pass
+
+    def add_group_event(self, group):
+        pass
+
+
+
