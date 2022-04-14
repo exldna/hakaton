5f1f564df4dcb7d80d0ead2540334d12fec33dd0
exldna <dm.a.titov@ya.ru>
2022-04-14 11:19:06 +0300

    bot say Hi!

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import source.bot
 
 def main():
     bot = source.bot.Bot()
-    
 
 if __name__ == "__main__":
     main()
--- source/bot.py
+++ source/bot.py
@@ -1,6 +1,13 @@
-import telebot
+from telebot import TeleBot
 
 class Bot:
     def __init__(self) -> None:
-        self.token = 
-        self.bot = telebot.Telebot()
+        # TODO: заприватить токен
+        self.token = "5378053989:AAFbBy8S8zD5MxSuOBU9eX58LmxefFOkLFA"
+        self.bot = TeleBot(self.token)
+
+        @self.bot.message_handler(commands=["start"])
+        def start_message(message):
+            self.bot.send_message(message.chat.id, "Hi!")
+
+        self.bot.infinity_polling()
