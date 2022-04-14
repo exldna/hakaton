07ee8ba8cb7716c6e26284b30ee3553f98ed1d91
exldna <dm.a.titov@ya.ru>
2022-04-14 16:30:38 +0300

    restruct classes

--- source/database.py
+++ source/database.py
@@ -1,44 +1,51 @@
 import sqlite3
 
 from threading import Thread
-from queue import Queue
+from multiprocessing import Queue
+
+from time import sleep
 
 class DataBase(object):
     def __init__(self, name: str = "db") -> None:
 
+        self.connect = sqlite3.connect(name + ".sqlite", check_same_thread=False)
+        self.cursor = self.connect.cursor()
+
         def run():
-            self.connect = sqlite3.connect(name + ".sqlite")
             # TODO: multiple cursors
-            self.cursor = self.connect.cursor()
             while self.is_running:
                 item = self.queue.get()
-                self.last_ans = self.__execute(item)
-                self.is_get = False
+                if item != "":
+                    self.last_ans = self.__execute(item)
+                    self.is_get = False
 
-        self.queue = Queue()
-        self.thread = Thread(target=run)
+        # self.queue = Queue()
+        # self.thread = Thread(target=run)
 
         self.is_running = True
         self.is_get = True
         self.last_ans = None
 
-        self.thread.start()
+        # self.thread.start()
 
     def __del__(self) -> None:
+        # TODO: блокировка!
         self.connect.close()
         self.is_running = False
-        self.thread.join()
+        # self.thread.join()
+        # self.queue.close()
 
-    def execute(self, request: str):
-        self.queue.put(request)
+    def __execute(self, request: str):
+        # self.queue.put(request)
         return self.get()
 
     def get(self):
-        if not self.is_get:
-            return self.last_ans
-            self.is_get = True
+        while self.is_get:
+            sleep(0.01)
+        self.is_get = True
+        return self.last_ans
 
-    def __execute(self, request: str):
+    def execute(self, request: str):
         self.cursor.execute(request)
         self.connect.commit()
         return self.cursor.fetchall()
